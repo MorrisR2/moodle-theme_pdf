d482ea794b14e894ff715ce644d66008b61cafd1
Kyle Temkin <ktemkin@binghamton.edu>
2012-09-06 14:02:01 -0400

    BU-68 added version.php to pdf theme

---
+++ version.php
@@ -0,0 +1,32 @@
+<?php
+ // This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version information for the true/false question type.
+ *
+ * @package    theme
+ * @subpackage pdf
+ * @copyright  2012 Binghamton University
+ * @author     Kyle J. Temkin <ktemkin@binghamton.edu>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+ 
+$plugin->version   = 2012082900;
+$plugin->component = 'theme_pdf';
+$plugin->requires  = 2010122500;
+$plugin->maturity  = MATURITY_BETA;
