d4ba710d9d6bcf854a3830098762030efc5658b6
Ray Morris <Ray.Morris@teex.tamu.edu>
2015-02-26 10:47:26 -0600

    change short tag <? to full tag <?php

--- pdf_renderers/core_question_pdf_renderer.php
+++ pdf_renderers/core_question_pdf_renderer.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 // Include the core dependencies for each of the question types.
 include_once($CFG->dirroot . '/question/engine/renderer.php');
@@ -109,4 +109,30 @@ class core_question_pdf_renderer extends core_question_renderer
     }
 
 
+    /**
+     * Generate the information bit of the question display that contains the
+     * metadata like the question number, current state, and mark.
+     * @param question_attempt $qa the question attempt to display.
+     * @param qbehaviour_renderer $behaviouroutput the renderer to output the behaviour
+     *      specific parts.
+     * @param qtype_renderer $qtoutput the renderer to output the question type
+     *      specific parts.
+     * @param question_display_options $options controls what should and should not be displayed.
+     * @param string|null $number The question number to display. 'i' is a special
+     *      value that gets displayed as Information. Null means no number is displayed.
+     * @return HTML fragment.
+     */
+    protected function info(question_attempt $qa, qbehaviour_renderer $behaviouroutput,
+            qtype_renderer $qtoutput, question_display_options $options, $number) {
+        $output = '';
+        $output .= $this->number($number);
+        $output .= "\n" . __FILE__ . ' ' . __LINE__ . "<br />\n";
+        $output .= $this->status($qa, $behaviouroutput, $options);
+        $output .= "\n" . __FILE__ . ' ' . __LINE__ . "<br />\n";
+        $output .= $this->mark_summary($qa, $behaviouroutput, $options);
+        $output .= "\n" . __FILE__ . ' ' . __LINE__ . "<br />\n";
+        $output .= $this->question_flag($qa, $options->flags);
+        $output .= $this->edit_question_link($qa, $options);
+        return $output;
+    }
 }
